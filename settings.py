84e9d3d675c6e80025757075da445dc4f9f046f5
Kacper <propozycja>
2020-05-12 22:24:11 +0100

    Initial commit

---
+++ settings.py
@@ -0,0 +1,13 @@
+class Settings():
+    def __init__(self):
+        self.windowWidth = 1200
+        self.windowHeight = 800
+        self.ship_speed_factor = 2
+        BLUE = (34, 36, 48)
+
+        self.background_color = BLUE
+
+        self.bullet_speed_factor = 1
+        self.bullet_width = 15
+        self.bullet_height = 3
+        self.bullet_color = 60,60,60
